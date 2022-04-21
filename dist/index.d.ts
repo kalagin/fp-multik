9e4dae2de23874e6119ec0acd6f1c605b949fd10
Ivan Kalagin <lulldev@yandex.ru>
2022-04-21 21:15:18 +0300

    Update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,69 @@
-export default function multik<T = unknown, S = any, R = unknown>(selectorFn: (arg: T) => S, ...predicateAsAction: Array<[predicateValue: S, action: (arg: T, selector: S) => R] | [predicateOr: S[], action: (arg: T, selector: S) => R] | [predicate: (arg: T, selector: S) => boolean, action: (arg: T, selector: S) => R] | [action: (arg: T, selector: S) => R]>): (arg: T) => R;
+export declare function multik<S, R, T1>(selectorFn: (arg1: T1) => S, ...predicateAsAction: Array<[predicateValue: S, action: (selector: S, arg1: T1) => R] | [predicateOr: S[], action: (selector: S, arg1: T1) => R] | [
+    predicate: (selector: S, arg1: T1) => boolean,
+    action: (selector: S, arg1: T1) => R
+] | [action: (selector: S, arg1: T1) => R]>): (arg1: T1) => R;
+export declare function multik<S, R, T1, T2>(selectorFn: (arg1: T1, arg2: T2) => S, ...predicateAsAction: Array<[predicateValue: S, action: (selector: S, arg1: T1, arg2: T2) => R] | [predicateOr: S[], action: (selector: S, arg1: T1, arg2: T2) => R] | [
+    predicate: (selector: S, arg1: T1, arg2: T2) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2) => R
+] | [action: (selector: S, arg1: T1, arg2: T2) => R]>): (arg1: T1, arg2: T2) => R;
+export declare function multik<S, R, T1, T2, T3>(selectorFn: (arg1: T1, arg2: T2, arg3: T3) => S, ...predicateAsAction: Array<[predicateValue: S, action: (selector: S, arg1: T1, arg2: T2, arg3: T3) => R] | [predicateOr: S[], action: (selector: S, arg1: T1, arg2: T2, arg3: T3) => R] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3) => R
+] | [action: (selector: S, arg1: T1, arg2: T2, arg3: T3) => R]>): (arg1: T1, arg2: T2, arg3: T3) => R;
+export declare function multik<S, R, T1, T2, T3, T4>(selectorFn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => S, ...predicateAsAction: Array<[
+    predicateValue: S,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R
+] | [
+    predicateOr: S[],
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R
+] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R
+] | [action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R]>): (arg1: T1, arg2: T2, arg3: T3, arg4: T4) => R;
+export declare function multik<S, R, T1, T2, T3, T4, T5>(selectorFn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => S, ...predicateAsAction: Array<[
+    predicateValue: S,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => R
+] | [
+    predicateOr: S[],
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => R
+] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => R
+] | [action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => R]>): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5) => R;
+export declare function multik<S, R, T1, T2, T3, T4, T5, T6>(selectorFn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => S, ...predicateAsAction: Array<[
+    predicateValue: S,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => R
+] | [
+    predicateOr: S[],
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => R
+] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => R
+] | [
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => R
+]>): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6) => R;
+export declare function multik<S, R, T1, T2, T3, T4, T5, T6, T7>(selectorFn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => S, ...predicateAsAction: Array<[
+    predicateValue: S,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => R
+] | [
+    predicateOr: S[],
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => R
+] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => R
+] | [
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => R
+]>): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7) => R;
+export declare function multik<S, R, T1, T2, T3, T4, T5, T6, T7, T8>(selectorFn: (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => S, ...predicateAsAction: Array<[
+    predicateValue: S,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => R
+] | [
+    predicateOr: S[],
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => R
+] | [
+    predicate: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => boolean,
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => R
+] | [
+    action: (selector: S, arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => R
+]>): (arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5, arg6: T6, arg7: T7, arg8: T8) => R;
+export default multik;
--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.multik = void 0;
 const utils_1 = require("./utils");
 function multik(selectorFn, ...predicateAsAction) {
     const predicates = Object.fromEntries(predicateAsAction);
     return (...args) => {
         const selector = selectorFn(...args);
         if (predicates[selector] && (0, utils_1.isPrimitive)(selector)) {
-            return predicates[selector](...args, selector);
+            return predicates[selector](selector, ...args);
         }
         let defaultAction = utils_1.noop;
         for (let i = 0; i < predicateAsAction.length; i++) {
@@ -16,27 +17,28 @@ function multik(selectorFn, ...predicateAsAction) {
                 continue;
             }
             if (typeof predicate === 'object' && (0, utils_1.deepEqual)(selector, predicate)) {
-                return action();
+                return action.call(null, selector, ...args);
             }
             if (predicateAsAction[i].length === 1 && typeof predicate === 'function') {
-                defaultAction = predicate.bind(null, ...args, selector);
+                defaultAction = predicate.bind(null, selector, ...args);
             }
             if (predicateAsAction[i].length === 2 &&
                 typeof predicate === 'function' &&
                 typeof action === 'function' &&
-                predicate(...args, selector)) {
-                return action();
+                predicate(selector, ...args)) {
+                return action.call(null, selector, ...args);
             }
             if (!(0, utils_1.isPrimitive)(selector) &&
                 Array.isArray(predicate) &&
                 predicate.includes(selector)) {
-                return action();
+                return action.call(null, selector, ...args);
             }
             if (Array.isArray(predicate) && predicate.some((p) => (0, utils_1.deepEqual)(p, selector))) {
-                return action();
+                return action.call(null, selector, ...args);
             }
         }
         return defaultAction();
     };
 }
+exports.multik = multik;
 exports.default = multik;
